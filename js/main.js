a8fb378a49bc00f1ae35fd0eccd86c8ff8f4113f
iris paredes <irisparedesjimenez@gmail.com>
2019-04-08 08:57:27 +0200

    create JS variables

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
 'use strict';
+
+let acc = document.querySelector('.acc');
+let number = document.querySelector('.number');
+const btn = document.querySelector('.btn');
+const feedback = document.querySelector('.feedback');
