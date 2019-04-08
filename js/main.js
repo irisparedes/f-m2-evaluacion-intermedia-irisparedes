4e556ee62564aed8b44103d98a8d1476cf5f41b8
iris paredes <irisparedesjimenez@gmail.com>
2019-04-08 09:41:40 +0200

    create functions

--- js/main.js
+++ js/main.js
@@ -4,3 +4,35 @@ let acc = document.querySelector('.acc');
 let number = document.querySelector('.number');
 const btn = document.querySelector('.btn');
 const feedback = document.querySelector('.feedback');
+
+
+
+// Función número aleatorio
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+  }
+  console.log('> ' + getRandomNumber(100));
+
+number = number.value;
+ 
+const random = getRandomNumber(100);
+/*for (let i = 1; i < 100; i++) {
+    acc[i].addEventListener("click", getRandomNumber); 
+  } Sin terminar!!! LO dejo para continuar...*/
+
+btn.addEventListener('click', getRandomNumber);
+if (number > random){
+    feedback.innerHTML = 'Demasiado alto';
+    getRandomNumber(100);
+    acc ++;
+} else if (number < random){
+    feedback.innerHTML = 'Demasiado alto';
+    getRandomNumber(100);
+    acc ++;
+}else {
+    feedback.innerHTML = '¡¡HAS GANADO CAMPEONA!!'
+    acc ++;
+};
+
+
+
